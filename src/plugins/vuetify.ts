24daa13e8457279d8be12f13d513f537042937f1
TwinHits <TwinHits@gmail.com>
2021-11-08 15:51:44 -0500

    More initial configuration and plugins for Vue, Vuetify, and Typescript.

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,11 @@
+// Vuetify configuration
+
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+const opts = {
+    theme: { disable: true },
+};
+
+export default new Vuetify(opts);
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,16 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  /*
   {
     path: "/",
-    name: "wrapper",
+    name: "home",
     component: Home,
   },
+  */
 ];
 
 const router = new VueRouter({
