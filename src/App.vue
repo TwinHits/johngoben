57d55115bd148033ad1ccfcf8d475902557326a9
John Goben <TwinHits@gmail.com>
2021-11-14 14:11:00 -0500

    Moved navigation to and bottom icons to parent level.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
         </v-navigation-drawer>
         <v-content app>
             <v-container fill-height class="">
-                <router-view />
+                <NextViewIcon class="up-next-view-icon" direction="up" />
+                    <router-view />
+                <NextViewIcon class="down-next-view-icon" direction="down" />
             </v-container>
         </v-content>
     </v-app>
@@ -13,17 +15,21 @@
 
 <script lang="ts">
 import Vue from 'vue';
+
 import LeftNavigation from '@/views/components/navigation/LeftNavigation.vue';
+import NextViewIcon from '@/views/components/navigation/NextViewIcon.vue';
 
 export default Vue.extend({
     components: {
         LeftNavigation,
+        NextViewIcon,
     },
 });
 </script>
 
 <style scoped lang="scss">
 @import "@/style/Colors.scss";
+@import "@/style/components/NextViewIcon.scss";
 .app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
