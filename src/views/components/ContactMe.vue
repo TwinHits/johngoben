9d2ff4314683e6caa9533584e5761d4d3fc30552
John Goben <TwinHits@gmail.com>
2022-01-28 13:42:11 -0500

    Filled, styled, and animated Contact Me.

--- src/views/components/ContactMe.vue
+++ src/views/components/ContactMe.vue
@@ -1,14 +1,29 @@
 <template>
-    <v-expand-transition>
-        <v-card @click="setShow(!show)" class="contact-me" :class="{ 'contact-me-full': show }">
-            <v-card-title>Contact Me</v-card-title>
-            <v-expand-transition>
-                <v-card-text v-if="show">
-                    You can contact me about professional oppurtunities via my linkedin or email
-                </v-card-text>
-            </v-expand-transition>
-        </v-card>
-    </v-expand-transition>
+    <v-card @click="setShow(!showFull)" class="contact-me" :class="{ 'contact-me-full': showFull }" elevation="10">
+        <v-card-title class="contact-me-title">Contact Me</v-card-title>
+        <v-fade-transition>
+            <v-row v-show="showText" align="center" justify="center">
+                <v-col class="contact-me-icon-col text-center" :cols="1">
+                    <v-icon size="35" color="#263238">mdi-at</v-icon>
+                </v-col>
+                <v-col class="contact-me-text" :cols="8">
+                    <a @click.stop="">john.goben@gmail.com</a>
+                </v-col>
+            </v-row>
+        </v-fade-transition>
+        <v-fade-transition>
+            <v-row v-show="showText" align="center" justify="center">
+                <v-col class="contact-me-icon-col text-center" :cols="1">
+                    <v-icon size="35" color="#263238">mdi-linkedin</v-icon>
+                </v-col>
+                <v-col class="contact-me-text" :cols="8">
+                    <a href="https://www.linkedin.com/in/johngoben/" target="_blank" @click.stop="">
+                        linkedin.com/in/johngoben/
+                    </a>
+                </v-col>
+            </v-row>
+        </v-fade-transition>
+    </v-card>
 </template>
 
 <script lang="ts">
@@ -17,12 +32,19 @@ import Vue from 'vue';
 export default Vue.extend({
     data() {
         return {
-            show: false,
+            showFull: false,
+            showText: false,
         };
     },
     methods: {
         setShow(show: boolean) {
-            this.show = show;
+            if (show) {
+                this.showFull = show;
+                setTimeout(() => (this.showText = show), 1000);
+            } else {
+                setTimeout(() => (this.showFull = show), 100);
+                this.showText = show;
+            }
         },
     },
 });
@@ -30,17 +52,33 @@ export default Vue.extend({
 
 <style scoped lang="scss">
 @import '@/style/Colors.scss';
+@import '@/style/Links.scss';
 
 .contact-me {
     background: $super-foreground;
     position: fixed;
     bottom: 2vh;
-    left: 90vw;
+    right: 1vw;
+    width: 6.9vw;
+    height: 6.5vh;
+    transition: width 1.2s, height 1.2s;
 }
 
 .contact-me-full {
-    background: $super-foreground;
-    max-width: 20vw;
-    left: 78vw;
+    width: 19vw;
+    height: 22vh;
+}
+
+.contact-me-title {
+    font-weight: bold;
+    font-size: 1.3em;
+}
+
+.contact-me-text {
+    font-size: 1.2em;
+}
+
+.contact-me-icon-col {
+    padding-right: 2vw;
 }
 </style>
