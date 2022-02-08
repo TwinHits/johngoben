2f895aae36a04e755c5c09738d265092e7494ad1
John Goben <TwinHits@gmail.com>
2022-02-08 17:59:52 -0500

    Fixed bad align value.

--- src/views/components/art/ArtDisplayCard.vue
+++ src/views/components/art/ArtDisplayCard.vue
@@ -1,7 +1,7 @@
 <template>
     <Transition name="fade-in" mode="out-in" appear>
         <v-card v-show="true || !loading" class="art-display-card" elevation="5">
-            <v-row class="art-img-container-row" no-gutters align="top">
+            <v-row class="art-img-container-row" no-gutters>
                 <v-col class="art-img-container-col">
                     <v-img
                         class="art-img"
