bf8e40bdb153ddd63e5ac7af498e374a455d2320
John Goben <TwinHits@gmail.com>
2022-02-18 16:47:12 -0500

    Fixed assets and document titel.

--- src/common/constants/common.ts
+++ src/common/constants/common.ts
@@ -1,1 +1,1 @@
-export const ASSETS_PATH = 'http://www.johngoben.com.s3-website-us-east-1.amazonaws.com/';
+export const ASSETS_PATH = 'http://www.johngoben.com.s3-website-us-east-1.amazonaws.com/assets/';
